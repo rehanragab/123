08ac3e314c71a197cda6ddb6f0dcecc0b082a03f
rehanragab <rr.shahen98@gmail.com>
2021-10-21 10:25:58 -0700

    test

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include"add.h"
+
+int main()
+{
+    int addd=0;
+    int x,y;
+
+   // scanf("%d",x);
+   // scanf("%d",y);
+    addd= add(10,20);
+    printf("%d",addd);
+    return 0;
+}
